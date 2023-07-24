0d78aa8d7ec56db761a8cc8bf4fe028b96a8f0da
AAA <AAA@DESKTOP-DT6AB8L>
2023-07-24 17:22:56 -0400

    add tesk

--- src/app/components/new-todo/new-todo.component.ts
+++ src/app/components/new-todo/new-todo.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Form } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { MatEndDate } from '@angular/material/datepicker';
+import { MatDialog } from '@angular/material/dialog';
+import { Title } from '@angular/platform-browser';
+import { ITodo } from 'src/app/modules/todo.interface';
+import { TodoService } from 'src/app/services/todo.service';
+import { v4 as uuidv4 } from 'uuid';
+
 
 @Component({
   selector: 'app-new-todo',
@@ -7,12 +14,26 @@ import { Form } from '@angular/forms';
   styleUrls: ['./new-todo.component.scss'],
 })
 export class NewTodoComponent implements OnInit {
-  @ViewChild('f') form: Form;
+  @ViewChild('f') form: NgForm;
 
-  constructor() {}
+  constructor(public dialog: MatDialog, private todoService: TodoService) {}
   ngOnInit(): void {}
 
   public onNewTodoSubmit(): void {
+    const formValue = this.form.form.value;
+
+    const newTodo: ITodo = {
+      id: uuidv4(),
+      title: formValue.title,
+      description: formValue.description,
+      isCompleted: false,
+      isArchived: false,
+      endDate: formValue.Date,
+      selected: false,
+    };
+
+    this.todoService.addNewTodo(newTodo);
+    this.dialog.closeAll()
     console.log('on submit');
     console.log(this.form);
   }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -7,95 +7,35 @@ import { ITodo } from '../modules/todo.interface';
   providedIn: 'root',
 })
 export class TodoService {
-  private mock: ITodo[] = [
-    {
-      id: 1,
-      title: 'Lion, mountain',
-      description: 'Felis concolor',
-      isCompleted: false,
-      isArchived: false,
-      endDate: '11/19/2022',
-      selected: true
-    },
-    {
-      id: 2,
-      title: 'Kelp gull',
-      description: 'Larus dominicanus',
-      isCompleted: false,
-      isArchived: false,
-      endDate: '5/24/2023',
-      selected: false
-    },
-    {
-      id: 3,
-      title: 'Golden-mantled ground squirrel',
-      description: 'Spermophilus lateralis',
-      isCompleted: false,
-      isArchived: true,
-      endDate: '8/12/2022',
-      selected: false
-    },
-    {
-      id: 4,
-      title: 'Savanna fox',
-      description: 'Dusicyon thous',
-      isCompleted: false,
-      isArchived: false,
-      endDate: '6/14/2023',
-      selected: false
-    },
-    {
-      id: 5,
-      title: 'Lion, mountain',
-      description: 'Felis concolor',
-      isCompleted: false,
-      isArchived: false,
-      endDate: '11/19/2022',
-      selected: false
-    },
-    {
-      id: 6,
-      title: 'Kelp gull',
-      description: 'Larus dominicanus',
-      isCompleted: false,
-      isArchived: false,
-      endDate: '5/24/2023',
-      selected: false
-    },
-    {
-      id: 7,
-      title: 'Golden-mantled ground squirrel',
-      description: 'Spermophilus lateralis',
-      isCompleted: false,
-      isArchived: true,
-      endDate: '8/12/2022',
-      selected: false
-    },
-    {
-      id: 8,
-      title: 'Savanna fox',
-      description: 'Dusicyon thous',
-      isCompleted: false,
-      isArchived: false,
-      endDate: '6/14/2023',
-      selected: false
-    },
-  ];
-  private _todoSubject: BehaviorSubject<Array<ITodo>> = new BehaviorSubject(this.mock);
+  private todos: Array<ITodo> = [];
 
-  private _singleTodoSubject: BehaviorSubject<ITodo> = new BehaviorSubject(this.mock[0])
+  private _todoSubject: BehaviorSubject<Array<ITodo>> = new BehaviorSubject(
+    this.todos
+  );
+
+  private _singleTodoSubject: BehaviorSubject<ITodo> = new BehaviorSubject(
+    this.todos.length ? this.todos[0] : null
+  );
 
   constructor() {}
 
-  public getTodos(): Observable<Array<ITodo>>{
-    return this._todoSubject.asObservable()
+  public getTodos(): Observable<Array<ITodo>> {
+    return this._todoSubject.asObservable();
   }
 
-  public getSelectedTodo():Observable<ITodo>{
-     return this._singleTodoSubject.asObservable()
+  public getSelectedTodo(): Observable<ITodo> {
+    return this._singleTodoSubject.asObservable();
   }
 
-  public setSelectedTodo(todo:ITodo){
-    this._singleTodoSubject.next(todo)
+  public setSelectedTodo(todo: ITodo) {
+    this._singleTodoSubject.next(todo);
+  }
+
+  public addNewTodo(newTodo: ITodo): void {
+    console.log(newTodo);
+    
+    const existingTodos: Array<ITodo> = this._todoSubject.value;
+    existingTodos.push(newTodo);
+    this._todoSubject.next(existingTodos);
   }
 }

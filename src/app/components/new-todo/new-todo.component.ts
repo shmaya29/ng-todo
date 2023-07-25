4695cd7b6b701b2d29f383f5f6d75526fa6a79a4
AAA <AAA@DESKTOP-DT6AB8L>
2023-07-25 10:58:47 -0400

    fixed 2 bug

--- src/app/components/new-todo/new-todo.component.ts
+++ src/app/components/new-todo/new-todo.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { MatEndDate } from '@angular/material/datepicker';
 import { MatDialog } from '@angular/material/dialog';
-import { Title } from '@angular/platform-browser';
 import { ITodo } from 'src/app/modules/todo.interface';
 import { TodoService } from 'src/app/services/todo.service';
 import { v4 as uuidv4 } from 'uuid';
@@ -14,6 +12,8 @@ import { v4 as uuidv4 } from 'uuid';
 })
 export class NewTodoComponent implements OnInit {
   @ViewChild('f') form: NgForm;
+
+  public minDate: Date = new Date()
 
   constructor(public dialog: MatDialog, private todoService: TodoService) {}
   ngOnInit(): void {}
